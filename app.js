baa8133b2843c90c75d6f503282077b6cdccf333
Aidan Simmons <aidanwsimmons621@gmail.com>
2024-04-24 17:44:34 -0700

    added functionality to build sketch grid

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-const screen = document.querySelector('screen')
+const screen = document.querySelector('.screen')
 
+const numRows = 16
+const numCols = 20
+
+function buildGrid(){
+    // Loop to create and append div elements for each cell in the grid
+    for (let i = 0; i < numRows * numCols; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        gridContainer.appendChild(cell);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", buildGrid())
