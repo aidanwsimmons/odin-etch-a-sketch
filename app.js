931b0de8c6f17bcb3c01c72f4ea2b8cd6974e9a2
Aidan Simmons <aidanwsimmons621@gmail.com>
2024-04-25 11:57:49 -0700

    fixed js and css to properly build grid

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const screen = document.querySelector('.screen')
 
-const numRows = 16
-const numCols = 20
+const numRows = 64
+const numCols = 64
 
 function buildGrid(){
     // Loop to create and append div elements for each cell in the grid
     for (let i = 0; i < numRows * numCols; i++) {
         const cell = document.createElement("div");
         cell.classList.add("cell");
-        gridContainer.appendChild(cell);
+        screen.appendChild(cell);
     }
 }
 
-document.addEventListener("DOMContentLoaded", buildGrid())
+document.addEventListener("DOMContentLoaded", buildGrid)
